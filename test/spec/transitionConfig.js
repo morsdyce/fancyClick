f962feca45e97504c0c8d5f2177ecca44ed35457
Maayan Glikser <maayan@glikm.com>
2014-10-10 15:49:16 +0300

    Add transition mode tests

--- test/spec/transitionConfig.js
+++ test/spec/transitionConfig.js
@@ -1,14 +1,62 @@
 describe('Transition config test', function() {
 
+    var async = new AsyncSpec(this);
+
     beforeEach(function() {
         loadFixtures('fixture.html');
     });
 
-    it('should add a class of fancy-leave to the existing container', function() {
+    async.it('should add a class of fancy-leave to the existing container', function(done) {
+
+        fancyClick.init({
+            animationMethod: 'transition'
+        });
+
+        spyOn($, 'ajax').andCallFake(function () {
+            return {
+                then: function (callback) {
+                    callback('<!doctype html>\n<html>\n<head>\n<title>New page title</title>\n</head>\n<body>\n<div class="main">\n    New page\n</div>\n</body>\n</html>');
+
+                    setTimeout(function() {
+                        var element = $('.fancy-leave');
+                        expect(element.length).toEqual(1);
+                        
+                        done();
+                    }, 100);
+
+                }
+            };
+        });
+
+        $('a:first').trigger('click');
 
     });
 
-    it('should add a container with a class of fancy-enter', function() {
+    async.it('should add a container with a class of fancy-enter', function(done) {
+
+        fancyClick.init({
+            container: '.main',
+            animationMethod: 'transition'
+        });
+
+        spyOn($, 'ajax').andCallFake(function () {
+            return {
+                then: function (callback) {
+                    callback('<!doctype html>\n<html>\n<head>\n<title>New page title</title>\n</head>\n<body>\n<div class="main">\n    New page\n</div>\n</body>\n</html>');
+
+                    setTimeout(function() {
+                        var element = $('.fancy-enter');
+                        expect(element.length).toEqual(1);
+                        expect(element.hasClass('main')).toBeTruthy();
+
+                        done();
+                    }, 100);
+
+                }
+            };
+        });
+
+        $('a:first').trigger('click');
 
     });
 
